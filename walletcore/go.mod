832a6e07841d57119208c9fd6143bd9a2d4e42eb
Luis Daniel Assulfi <lassulfi@gmail.com>
2023-02-02 20:49:52 -0300

    finalizado curso arquietura de microsservicos

---
+++ walletcore/go.mod
@@ -0,0 +1,9 @@
+module github.com/lassulfi/fc-ms-wallet
+
+go 1.15
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/mattn/go-sqlite3 v1.14.16
+	github.com/stretchr/testify v1.8.1
+)
---
+++ walletcore/internal/usecase/create_transaction/create_transaction.go
@@ -0,0 +1,50 @@
+package createtransaction
+
+import (
+	"github.com/lassulfi/fc-ms-wallet/internal/entity"
+	"github.com/lassulfi/fc-ms-wallet/internal/gateway"
+)
+
+type CreateTransactionInputDTO struct {
+	AccountIDFrom string
+	AccountIDTo   string
+	Amount        float64
+}
+
+type CreateTransactionOutputDTO struct {
+	ID string
+}
+
+type CreateTransactionUseCase struct {
+	TransactionGateway gateway.TransactionGateway
+	AccountGateway     gateway.AccountGateway
+}
+
+func NewCreateTransactionUseCase(t gateway.TransactionGateway, a gateway.AccountGateway) *CreateTransactionUseCase {
+	return &CreateTransactionUseCase{
+		TransactionGateway: t,
+		AccountGateway:     a,
+	}
+}
+
+func (uc *CreateTransactionUseCase) Execute(input CreateTransactionInputDTO) (*CreateTransactionOutputDTO, error) {
+	accountFrom, err := uc.AccountGateway.FindById(input.AccountIDFrom)
+	if err != nil {
+		return nil, err
+	}
+	accountTo, err := uc.AccountGateway.FindById(input.AccountIDTo)
+	if err != nil {
+		return nil, err
+	}
+	transaction, err := entity.NewTransaction(accountFrom, accountTo, input.Amount)
+	if err != nil {
+		return nil, err
+	}
+	err = uc.TransactionGateway.Create(transaction)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateTransactionOutputDTO{
+		ID: transaction.ID,
+	}, nil
+}
